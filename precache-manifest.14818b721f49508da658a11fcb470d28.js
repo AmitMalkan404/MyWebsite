93a710136553b13d58464842762242fe5ad1fae8
Amit Malkan <amit.malkan@gmail.com>
2020-09-25 19:55:07 +0300

    Updates

---
+++ precache-manifest.14818b721f49508da658a11fcb470d28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10082b4e9b163e970cfe54c2e14e04f6",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "8916e2049e39680feb45",
+    "url": "/MyWebsite/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8df576f48f1a79469f8b",
+    "url": "/MyWebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8916e2049e39680feb45",
+    "url": "/MyWebsite/static/js/2.6cdfeb3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyWebsite/static/js/2.6cdfeb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df576f48f1a79469f8b",
+    "url": "/MyWebsite/static/js/main.23bf183c.chunk.js"
+  },
+  {
+    "revision": "aa005ab8219e6eaff1ec",
+    "url": "/MyWebsite/static/js/runtime-main.2ec9ee0e.js"
+  },
+  {
+    "revision": "72a25b12b2ec1ebca9bf6b39c588825f",
+    "url": "/MyWebsite/static/media/my-photo.72a25b12.jpg"
+  }
+]);
