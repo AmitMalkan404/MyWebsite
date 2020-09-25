40b949922f57533481d13f0584de91b19b49dfcd
Amit Malkan <amit.malkan@gmail.com>
2020-09-25 19:50:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
 import './App.css';
+import { Nav, Navbar } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Slide,Fade,Zoom } from "react-awesome-reveal";
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+import ContactPage from './pages/ContactPage';
+
+//my app
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Amit Malkan',
+      headLinks: [
+        {title: 'Home', path: '/' },
+        {title: 'About', path: '/about' },
+        {title: 'Projects', path: '/projects' },
+        {title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Open for work!',
+        subTitle: 'Success is achieved by ordinary people with extraordinary determintaion',
+      },
+      about: {
+        title: 'About me'
+      },
+      projects: {
+        title: 'Here are some of my works'
+      },
+      contact: {
+        title: 'Let\'s stay on touch!'
+      }
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Zoom>
+            <Navbar className="border-bottom" bg="transparent" expand="lg">
+              <Navbar.Brand>Amit Malkan</Navbar.Brand>
+              
+              <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+              <Navbar.Collapse id="navbar-toggle">
+                <Nav className="ml-auto">
+                  <Link className="nav-link" to="/MyWebsite/">Home</Link>
+                  <Link className="nav-link" to="/MyWebsite/about">About</Link>
+                  <Link className="nav-link" to="/MyWebsite/projects">Projects</Link>
+                  <Link className="nav-link" to="/MyWebsite/contact">Contact</Link>
+                  
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </Zoom>
+          <Route path="/MyWebsite/" exact render={() => <Fade><HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} /></Fade>} />
+          <Route path="/MyWebsite/about" exact render={() => <Fade><AboutPage title={this.state.about.title} /></Fade>} />
+          <Route path="/MyWebsite/projects" exact render={() => <Fade><ProjectsPage title={this.state.projects.title} /></Fade>} />
+          <Route path="/MyWebsite/contact" exact render={() => <Fade><ContactPage title={this.state.contact.title} /></Fade>} />
+
+          <Footer/>
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/ProjectsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+function ProjectsPage(props) {
+
+    return(
+        <Hero title={props.title} />
+    );
+
+}
+
+export default ProjectsPage;
